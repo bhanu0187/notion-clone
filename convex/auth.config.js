0a60a68c007e9330832cc082c228dc3c1eb94942
BhanuPratap-Singh-Naruka <pratapbhanu0187@gmail.com>
2023-11-26 13:41:38 +0530

    set up convex for backend and clerk for authentication

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+	providers: [
+		{
+			domain: "https://mutual-tomcat-80.clerk.accounts.dev",
+			applicationID: "convex",
+		},
+	],
+};
